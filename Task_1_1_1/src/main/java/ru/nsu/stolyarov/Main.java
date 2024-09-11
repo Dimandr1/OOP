24c40821a0a24bdde8ad01caed7b203b30b2f696
Stolyarov Dmitry <d.stolyarov@g.nsu.ru>
2024-09-11 20:34:00 +0700

    test

--- Task_1_1_1/src/main/java/ru/nsu/stolyarov/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/stolyarov/Main.java
@@ -3,25 +3,25 @@ package ru.nsu.stolyarov;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void CheckHeap(int[] arr, int n, int i){
-        if(i*2 + 2 < n && arr[i] < arr[i*2+2] && arr[i*2+1] < arr[i*2+2]){
+    public static void CheckHeap(int[] arr, int n, int i) {
+        if (i * 2 + 2 < n && arr[i] < arr[i * 2 + 2] && arr[i * 2 + 1] < arr[i * 2 + 2]) {
             int t = arr[i];
-            arr[i] = arr[i*2+2];
-            arr[i*2+2] = t;
-            CheckHeap(arr, n, i*2+2);
-        }
-        else if(i*2 + 1 < n && arr[i] < arr[i*2+1]){
+            arr[i] = arr[i * 2 + 2];
+            arr[i * 2 + 2] = t;
+            CheckHeap(arr, n, i * 2 + 2);
+        } else if (i * 2 + 1 < n && arr[i] < arr[i * 2 + 1]) {
             int t = arr[i];
-            arr[i] = arr[i*2+1];
-            arr[i*2+1] = t;
-            CheckHeap(arr, n, i*2+1);
+            arr[i] = arr[i * 2 + 1];
+            arr[i * 2 + 1] = t;
+            CheckHeap(arr, n, i * 2 + 1);
         }
     }
-    public static int[] Heapsort(int[] arr){
-        for(int i = arr.length - 1; i >= 0; i--){
+
+    public static int[] Heapsort(int[] arr) {
+        for (int i = arr.length - 1; i >= 0; i--) {
             CheckHeap(arr, arr.length, i);
         }
-        for(int i = arr.length - 1; i >= 0; i--){
+        for (int i = arr.length - 1; i >= 0; i--) {
             int t = arr[0];
             arr[0] = arr[i];
             arr[i] = t;
