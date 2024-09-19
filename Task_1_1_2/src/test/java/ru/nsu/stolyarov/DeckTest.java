ef4d8d048d418ec471f2659fb7f5f631690fbb5e
Stolyarov Dmitry <d.stolyarov@g.nsu.ru>
2024-09-19 17:41:19 +0700

    Task_1_1_2

--- Task_1_1_2/src/test/java/ru/nsu/stolyarov/DeckTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/stolyarov/DeckTest.java
@@ -3,13 +3,14 @@ package ru.nsu.stolyarov;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class DeckTest {
     @Test
-    void test(){
+    void test() {
         Deck testDeck = new Deck();
         testDeck.remakeDeck(1);
         testDeck.remakeDeck(5);
         testDeck.remakeDeck(10);
-        testDeck.getTopCard();  
+        testDeck.getTopCard();
     }
 }
