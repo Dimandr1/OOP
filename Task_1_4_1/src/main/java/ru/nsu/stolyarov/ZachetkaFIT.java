25356ed5c434a477826d661d5df0d40f3bdc7069
Dimandr <Dmitry221556@yandex.ru>
2024-11-20 13:21:27 +0700

    Task_1_4_1

---
+++ Task_1_4_1/src/main/java/ru/nsu/stolyarov/ZachetkaFIT.java
@@ -0,0 +1,40 @@
+package ru.nsu.stolyarov;
+
+/**
+ * Частный случай зачётки (ФИТ).
+ */
+public class ZachetkaFIT extends Zachetka {
+    /**
+     * Инициализация зачётки с генерацией дисциплин ФИТа.
+     *
+     * @param studyFree на бюджете ли студент
+     */
+    public ZachetkaFIT(boolean studyFree) {
+        super(studyFree);
+
+        AddZachet("Английский", "Зачёт", 1);
+        AddZachet("История", "Зачёт", 2);
+        AddZachet("ФКиС", "Зачёт", 3);
+        AddZachet("Откис", "Зачёт", 4, false);
+        AddZachet("Прокис", "Зачёт", 5, false);
+        AddZachet("Инфобез", "Зачёт", 6);
+        AddZachet("ОБЖ", "Зачёт", 8);
+
+        AddDifZachet("аЛСД", "Дифзачёт", 1, false);
+        AddDifZachet("аЛСД", "Дифзачёт", 2, false);
+        AddDifZachet("История", "Дифзачёт", 1);
+        AddDifZachet("Дискретка", "Экзамен", 1);
+        AddDifZachet("Дискретка", "Экзамен", 2);
+        AddDifZachet("ООП", "Дифзачёт", 3);
+        AddDifZachet("ООП", "Экзамен", 4);
+        AddDifZachet("Оси", "Дифзачёт", 3);
+        AddDifZachet("Оси", "Экзамен", 3);
+        AddDifZachet("ИИ", "Экзамен", 3);
+        AddDifZachet("Английский", "Дифзачёт", 5);
+        AddDifZachet("ИИ", "Экзамен", 5);
+        AddDifZachet("DL", "Дифзачёт", 6);
+        AddDifZachet("Инфобез", "Дифзачёт", 7);
+        AddDifZachet("Философия", "Дифзачёт", 8);
+        AddDifZachet("ВКР", "Защита ВКР", 8);
+    }
+}
---
+++ Task_1_4_1/src/test/java/ru/nsu/stolyarov/ZachetkaFITTest.java
@@ -0,0 +1,39 @@
+package ru.nsu.stolyarov;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ZachetkaFITTest {
+    @Test
+    void test() {
+        ZachetkaFIT zachetka = new ZachetkaFIT(false);
+
+        zachetka.SetPass("Английский", 1, true);
+        zachetka.SetPass("История", 2, true);
+
+        zachetka.SetMark("аЛСД", 1, 3);
+        zachetka.SetMark("аЛСД", 2, 5);
+        zachetka.SetMark("История", 1, 5);
+        zachetka.SetMark("Дискретка", 1, 4);
+        zachetka.SetMark("Дискретка", 2, 3);
+
+        assertEquals(4f, zachetka.GetAverageMark());
+        assertEquals(4.5, zachetka.GetAverageDipMark());
+        assertEquals(false, zachetka.MayStudyFree());
+        assertEquals(false, zachetka.MayRecieveGrant());
+        zachetka.SetMark("Дискретка", 2, 4);
+        assertEquals(true, zachetka.MayStudyFree());
+        assertEquals(true, zachetka.MayRecieveGrant());
+        assertEquals(true, zachetka.MayGetRed());
+        zachetka.SetMark("ИИ", 5, 4);
+        zachetka.SetMark("DL", 6, 4);
+        zachetka.SetMark("Инфобез", 7, 4);
+        zachetka.SetMark("Философия", 8, 4);
+        assertEquals(false, zachetka.MayGetRed());
+
+
+        zachetka.AddZachet("***", "Зачёт", 5, false);
+        zachetka.AddDifZachet("ОБЖ", "Экзамен", 1);
+    }
+}
