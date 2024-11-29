0f20ea25be658f5efe51a6a63ee46d8c72911ac3
Dimandr <Dmitry221556@yandex.ru>
2024-11-29 08:41:27 +0700

    Task_1_4_1

--- Task_1_4_1/src/test/java/ru/nsu/stolyarov/ZachetkaFITTest.java
+++ Task_1_4_1/src/test/java/ru/nsu/stolyarov/ZachetkaFITTest.java
@@ -1,8 +1,9 @@
 package ru.nsu.stolyarov;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class ZachetkaFITTest {
     @Test
