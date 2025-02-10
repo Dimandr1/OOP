7c6737ca758019e63bc6322b3a7a3c43151fcb87
Dimandr <Dmitry221556@yandex.ru>
2025-02-10 16:36:37 +0700

    Task_2_1_1

--- Task_2_1_1/src/main/java/ru/nsu/stolyarov/Solver.java
+++ Task_2_1_1/src/main/java/ru/nsu/stolyarov/Solver.java
@@ -2,50 +2,51 @@ package ru.nsu.stolyarov;
 
 import java.util.ArrayList;
 
-public class Solver{
+public class Solver {
 
     protected ArrayList<Integer> digits;
     protected boolean b;
     protected int cur;
 
-    public boolean isSimple(int digit){
-        for(int i = 2; i * i <= digit; i++){
-            if(digit % i == 0){
+    public boolean isSimple(int digit) {
+        for (int i = 2; i * i <= digit; i++) {
+            if (digit % i == 0) {
                 return false;
             }
         }
         return true;
     }
 
-    public Solver(ArrayList<Integer> ar){
+    public Solver(ArrayList<Integer> ar) {
         digits = ar;
     }
+
     public boolean solve(int threadsAmount) throws InterruptedException {
         b = false;
         cur = 0;
-        
-        if(threadsAmount == 1){
+
+        if (threadsAmount == 1) {
             SolverThread mainThread = new SolverThread();
             mainThread.run();
         } else if (threadsAmount == 0) {
-            if(digits.parallelStream().map(n -> isSimple(n)).filter(n -> !n).toList().isEmpty()){
+            if (digits.parallelStream().map(n -> isSimple(n))
+                    .filter(n -> !n).toList().isEmpty()) {
                 return false;
-            }
-            else{
+            } else {
                 return true;
             }
 
         } else if (threadsAmount > 1) {
             ArrayList<SolverThread> threads = new ArrayList<>();
-            for(int i = 0; i < threadsAmount; i++) {
+            for (int i = 0; i < threadsAmount; i++) {
                 SolverThread anotherThread = new SolverThread();
                 anotherThread.id = i;
                 threads.add(anotherThread);
             }
-            for(int i = 0; i < threadsAmount; i++) {
+            for (int i = 0; i < threadsAmount; i++) {
                 threads.get(i).start();
             }
-            for(int i = 0; i < threadsAmount; i++) {
+            for (int i = 0; i < threadsAmount; i++) {
                 threads.get(i).join();
             }
         }
@@ -53,18 +54,19 @@ public class Solver{
         return b;
     }
 
-    private class SolverThread extends Thread{
+    private class SolverThread extends Thread {
         int id;
 
-        private synchronized int increment(){
+        private synchronized int increment() {
             cur++;
             return cur - 1;
         }
+
         @Override
-        public void run(){
-            while (!b && cur < digits.size()){
+        public void run() {
+            while (!b && cur < digits.size()) {
                 int t = increment();
-                if(t < digits.size() && !isSimple(digits.get(t))){
+                if (t < digits.size() && !isSimple(digits.get(t))) {
                     b = true;
                 }
             }
