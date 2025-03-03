33fe5c7a313370b5d06f08218995b4a4babf4355
Dimandr <Dmitry221556@yandex.ru>
2025-03-03 17:05:41 +0700

    Task_2_2_1

--- Task_2_2_1/src/main/java/ru/nsu/stolyarov/Main.java
+++ Task_2_2_1/src/main/java/ru/nsu/stolyarov/Main.java
@@ -1,13 +1,20 @@
 package ru.nsu.stolyarov;
 
+/**
+ * main lol.
+ */
 public class Main {
+    /**
+     * lol main.
+     * @param args
+     */
     public static void main(String[] args) {
-        Configuration myConf = new Configuration();
+       /* Configuration myConf = new Configuration();
         myConf.bakers = new int[]{100, 200, 300, 50, 150};
         myConf.carriers = new int[]{2, 1, 3, 1, 2};
         myConf.storageCapacity = 20;
         myConf.workingTime = 10000;
-        myConf.sleepTime = 3000;
+        myConf.sleepTime = 3000;*/
 
     }
 }
